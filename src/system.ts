cd0432db58d8c21c25de8937ae88fad1c2fb8c3b
waixuexi <59311567+Waixuexi994@users.noreply.github.com>
2023-05-16 15:07:56 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -11,7 +11,7 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `微信公众号：书虫创客
-- 网站以进行更改，现在网站不需要填写key即可使用，网站余额没了或者有其他问题添加书虫微信：SCFuYe001
+- 网站以进行更改，网站余额购买或者有其他问题添加书虫微信：SCFuYe001
 - https://www.fakabang.com/links/3E277A6E
 - ↑↑↑，上面链接可以直接购买ChatGPT个人独享账号，自动发货，也可找书虫购买，免去手续费，手续费由平台收取
 - 现在国内镜像版版本，每分钟只能回答三次，想购买独立key或者ChatGPT官方账号也可以联系书虫微信：SCFuYe001
